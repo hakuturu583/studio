e351f8dc140e9074b131c93703d81893db61330d
John Hurliman <jhurliman@jhurliman.org>
2021-05-28 16:37:03 -0700

    Add an integration test to check the total production build size

---
+++ desktop/test/buildSize.test.ts
@@ -0,0 +1,40 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { readdir, stat } from "fs/promises";
+import * as path from "path";
+
+// Adjust this byte size as needed if the app is growing for a valid reason.
+// This check is here to catch unexpected ballooning of the build size
+const MAX_BUILD_SIZE = 100_000_000;
+
+async function getAllFiles(dirPath: string, arrayOfFiles?: string[]): Promise<string[]> {
+  const files = await readdir(dirPath, { withFileTypes: true });
+  arrayOfFiles = arrayOfFiles ?? [];
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      arrayOfFiles = await getAllFiles(path.join(dirPath, file.name), arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, file.name));
+    }
+  }
+
+  return arrayOfFiles;
+}
+
+async function getTotalSize(directoryPath: string): Promise<number> {
+  const arrayOfFiles = await getAllFiles(directoryPath);
+  let totalSize = 0;
+  for (const filename of arrayOfFiles) {
+    totalSize += (await stat(filename)).size;
+  }
+  return totalSize;
+}
+
+it("build size should not grow significantly", async () => {
+  const buildDir = path.join(__dirname, "..", ".webpack");
+  const buildSizeInBytes = await getTotalSize(buildDir);
+  expect(buildSizeInBytes).toBeLessThan(MAX_BUILD_SIZE);
+});
