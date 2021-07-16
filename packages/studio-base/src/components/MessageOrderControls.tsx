e8cf2067deb424d9f06a7a007bea8b73abb0406c
Andrew Carr <andrew@2metres.com>
2021-07-16 21:24:31 +1000

    Fix up comments

--- packages/studio-base/src/components/MessageOrderControls.tsx
+++ packages/studio-base/src/components/MessageOrderControls.tsx
@@ -1,9 +1,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
-//
-// This file incorporates work covered by the following copyright and
-// permission notice:
 
 import { DefaultButton, useTheme } from "@fluentui/react";
 import { useCallback } from "react";
